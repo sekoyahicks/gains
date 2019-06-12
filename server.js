35143a63071a0b75d84e8e92ba09e44e27d0ad9c
Sekoya Hicks <sekoya91@yahoo.com>
2019-06-12 18:53:55 -0400

    Added routes to server.js file

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
-const express = require('express')
-const logger = require('morgan')
-const app = express()
-
-app.use(logger('dev'))
-app.use(express.urlencoded({ extended: true }));
+const express = require("express");
+const logger = require("morgan");
+const router = express.Router();
+const app = express();
+const userRoute = require("./routes/userRoute");
+const caloriesRoute = require("./routes/caloriesRoute");
+const weightRoute = require("./routes/weightRoute");
 app.use(express.json());
-app.use(express.static(__dirname + '/client/build/'));
-app.get('/', (req,res) => {
-    res.sendFile(__dirname + '/client/build/index.html')
-})
 
-app.get('/test', (req, res) => {
-  res.send("Hello World")
-})
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 3001
+app.use(express.static(__dirname + "/client/build/"));
+app.get("/*", (req, res) => {
+  res.sendFile(__dirname + "/client/build/index.html");
+});
+
+app.use("/user", userRoute);
+app.use("/calories", caloriesRoute);
+app.use("weight", weightRoute);
+
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log('App is up and running on port ' + PORT)
-})
+  console.log("App is up and running on port " + PORT);
+});
