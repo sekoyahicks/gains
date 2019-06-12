a4c4cf8e7c20c60e8acc2d11a4eabf6d467e9c57
Sekoya Hicks <sekoya91@yahoo.com>
2019-06-12 18:43:17 -0400

    Added routes

---
+++ routes/caloriesRoute.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+const caloriesController = require('../controllers/caloriesController')
+
+router.get('/', caloriesController.index)
+router.post('/', caloriesController.create)
+router.get('/:id', caloriesController.show)
+router.patch('/:id', caloriesController.update)
+router.delete('/:id', caloriesController.delete)
+
+module.exports = router;
---
+++ routes/weightRoute.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+const weightController = require('../controllers/weightController')
+
+router.get('/', weightController.index)
+router.post('/', weightController.create)
+router.get('/:id', weightController.show)
+router.patch('/:id', weightController.update)
+router.delete('/:id', weightController.delete)
+
+module.exports = router;
